7708e01c7ad74388daf6277d907ec9b806573c5d
zmartl-bbc <luca.marti@bluewin.ch>
2016-02-04 21:52:58 +0100

    Webpage

---
+++ imprint.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Luca Marti's Webpage</title>
+
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+<!-- Custom Fonts -->
+<link
+	href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+	rel='stylesheet' type='text/css'>
+<link
+	href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+	rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"
+	type="text/css">
+
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="css/creative.css" type="text/css">
+
+
+<link rel="stylesheet" href="css/app.css" type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Navigation einklappen</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand page-scroll" href="#page-top">Luca Marti</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse"
+				id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a class="page-scroll" href="index.php#masters">Team Masters
+							Project</a></li>
+					<li><a class="page-scroll" href="index.php#airport">Flughafen
+							Z&uuml;rich AG</a></li>
+					<li><a class="page-scroll" href="index.php#project">Projekte</a></li>
+					<li><a class="page-scroll" href="index.php#contact">Kontakt</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+
+	<header>
+		<div class="header-content">
+			<div class="header-content-inner">
+				<h1>Herzlich Willkommen auf luca-marti.ch</h1>
+				<hr>
+				<p>Auf dieser Webseite finden Sie Informationen zu Projekten,
+					Portfolios und mehr!</p>
+				<a href="index.php#project" class="btn btn-primary btn-xl page-scroll">Let's go</a>
+			</div>
+		</div>
+	</header>
+	<aside class="bg-dark">
+		<div class="container text-center">
+			<div class="call-to-action">
+				<h2>Impressum</h2>
+				<hr class="primary">
+				<div class="impressum">
+					<h2>Kontaktadresse</h2>
+					<p>admin@luca-marti.ch</p>
+					<p>&nbsp;</p>
+					<h2>Haftungsausschluss</h2>
+					<p>Der Autor &uuml;bernimmt keinerlei Gew&auml;hr hinsichtlich der
+						inhaltlichen Richtigkeit, Genauigkeit, Aktualit&auml;t, Zuverl&auml;ssigkeit
+						und Vollst&auml;ndigkeit der Informationen.</p>
+					<p>Haftungsanspr&uuml;che gegen den Autor wegen Sch&auml;den materieller oder
+						immaterieller Art, welche aus dem Zugriff oder der Nutzung bzw.
+						Nichtnutzung der ver&ouml;ffentlichten Informationen, durch Missbrauch
+						der Verbindung oder durch technische St&ouml;rungen entstanden sind,
+						werden ausgeschlossen.</p>
+					<p>Alle Angebote sind unverbindlich. Der Autor beh&auml;lt es sich
+						ausdr&uuml;cklich vor, Teile der Seiten oder das gesamte Angebot ohne
+						gesonderte Ank&uuml;ndigung zu ver&auml;ndern, zu erg&auml;nzen, zu l&ouml;schen oder
+						die Ver&ouml;ffentlichung zeitweise oder endg&uuml;ltig einzustellen.</p>
+					<p>&nbsp;</p>
+					<h2>Haftung f&uuml;r Links</h2>
+					<p>Verweise und Links auf Webseiten Dritter liegen ausserhalb
+						unseres Verantwortungsbereichs Es wird jegliche Verantwortung f&uuml;r
+						solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher
+						Webseiten erfolgen auf eigene Gefahr des Nutzers oder der
+						Nutzerin.</p>
+					<p>&nbsp;</p>
+					<h2>Urheberrechte</h2>
+					<p>
+						Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos
+						oder anderen Dateien auf der Website geh&ouml;ren ausschliesslich <strong></strong>
+						oder den speziell genannten Rechtsinhabern. F&uuml;r die Reproduktion
+						jeglicher Elemente ist die schriftliche Zustimmung der
+						Urheberrechtstr&auml;ger im Voraus einzuholen.
+					</p>
+					<p>&nbsp;</p>
+					<h2>Datenschutz</h2>
+					<p>Gest&uuml;tzt auf Artikel 13 der schweizerischen Bundesverfassung und
+						die datenschutzrechtlichen Bestimmungen des Bundes
+						(Datenschutzgesetz, DSG) hat jede Person Anspruch auf Schutz ihrer
+						Privatsph&auml;re sowie auf Schutz vor Missbrauch ihrer pers&ouml;nlichen
+						Daten. Wir halten diese Bestimmungen ein. Pers&ouml;nliche Daten werden
+						streng vertraulich behandelt und weder an Dritte verkauft noch
+						weiter gegeben.</p>
+					<p>In enger Zusammenarbeit mit unseren Hosting-Providern bem&uuml;hen
+						wir uns, die Datenbanken so gut wie m&ouml;glich vor fremden Zugriffen,
+						Verlusten, Missbrauch oder vor F&auml;lschung zu sch&uuml;tzen.</p>
+					<p>Beim Zugriff auf unsere Webseiten werden folgende Daten in
+						Logfiles gespeichert: IP-Adresse, Datum, Uhrzeit, Browser-Anfrage
+						und allg. &uuml;bertragene Informationen zum Betriebssystem resp.
+						Browser. Diese Nutzungsdaten bilden die Basis f&uuml;r statistische,
+						anonyme Auswertungen, so dass Trends erkennbar sind, anhand derer
+						wir unsere Angebote entsprechend verbessern k&ouml;nnen.</p>
+					<p>&nbsp;</p>
+					<h2>Quelle</h2>
+					<p>
+						Dieses Impressum wurde am <strong>04.02.2016</strong> mit dem
+						Impressum-Generator <strong><a
+							href="http://www.bag.ch/impressum-generator">http://www.bag.ch/impressum-generator</a></strong>
+						der Firma Brunner AG, Druck und Medien, in Kriens erstellt. Die
+						Brunner AG, Druck und Medien, in Kriens &uuml;bernimmt keine Haftung.
+					</p>
+					<p style="visibility: hidden;"></p>
+				</div>
+
+			</div>
+		</div>
+	</aside>
+
+	<aside class="bg-dark footer">
+		<div class="container text-center">
+			<div class="call-to-action">
+				<p>Copyright &copy; <?php echo date("Y") ?> luca-marti.ch | Luca Marti | <a
+						href="http://startbootstrap.com/" target="_blank">Bootstrap</a> |
+					<a href="http://startbootstrap.com/" target="_blank">Impressum</a>
+				</p>
+			</div>
+		</div>
+	</aside>
+
+
+
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Plugin JavaScript -->
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/jquery.fittext.js"></script>
+	<script src="js/wow.min.js"></script>
+
+	<!-- Custom Theme JavaScript -->
+	<script src="js/creative.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="luca-marti.ch">
+<meta name="author" content="Luca Marti">
+
+<title>Luca Marti's Webpage</title>
+
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+<!-- Custom Fonts -->
+<link
+	href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+	rel='stylesheet' type='text/css'>
+<link
+	href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+	rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"
+	type="text/css">
+
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="css/creative.css" type="text/css">
+
+
+<link rel="stylesheet" href="css/app.css" type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Navigation einklappen</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand page-scroll" href="#page-top">Luca Marti</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse"
+				id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a class="page-scroll" href="#masters">Team Masters Project</a></li>
+					<li><a class="page-scroll" href="#airport">Flughafen Z&uuml;rich AG</a></li>
+					<li><a class="page-scroll" href="#project">Projekte</a></li>
+					<li><a class="page-scroll" href="#contact">Kontakt</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+
+	<header>
+		<div class="header-content">
+			<div class="header-content-inner">
+				<h1>Herzlich Willkommen auf luca-marti.ch</h1>
+				<hr>
+				<p>Auf dieser Webseite finden Sie Informationen zu Projekten,
+					Portfolios und mehr!</p>
+				<a href="#project" class="btn btn-primary btn-xl page-scroll">Let's
+					go</a>
+			</div>
+		</div>
+	</header>
+
+	<section class="bg-primary" id="masters">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 text-center">
+					<div class="row">
+						<h2 class="section-heading">Masters Project</h2>
+						<hr class="light">
+					</div>
+					<div class="row">
+						<div class="col-md-5 col-sm-offset-1">
+							<div class="row">
+								<h3 class="section-heading">Quizmasters</h3>
+							</div>
+							<div class="row">
+								<p class="text-faded">Quizmasters ist eine Web-Anwendung, welche
+									in dem Basislehrjahr erstellt wurde. Es ist eine Webseite, auf
+									der man verschiedene Quiz l&ouml;sen und sich so mit der ganzen
+									Welt messen kann!</p>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<a href="https://quizmasters.ch/" target="_blank"
+										class="btn btn-default btn-xl"><i class="fa fa-desktop"></i>
+										Webseite</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-5 col-sm-offset-1">
+							<div class="row">
+								<h3 class="section-heading">Transportmasters</h3>
+							</div>
+							<div class="row">
+								<p class="text-faded">
+									Das Projekt <em>Transportmasters</em> ist eine Webanwendung,
+									welche die Zugverbindungen anzeigen kann. Die Anwendung wurde
+									mit einer Web-API realisiert!
+								</p>
+							</div>
+							<div class="row">
+								<div class="row">
+									<div class="col-md-12">
+										<a href="https://transport.quizmasters.ch/" target="_blank"
+											class="btn btn-default btn-xl"><i class="fa fa-desktop"></i>
+											Webseite</a>
+									</div>
+								</div>
+								<div class="row">
+									<p></p>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<a href="https://github.com/MastersProjects/TransportMasters"
+											target="_blank" class="btn btn-default btn-xl"><i
+											class="fa fa-github"></i> Github</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="airport">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h2 class="section-heading">Flughafen Z&uuml;rich AG</h2>
+					<hr class="primary">
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-md-6 text-center">
+					<div class="service-box">
+						<i class="fa fa-4x fa-book wow bounceIn text-primary"></i>
+						<h3>
+							<a href="" target="_blank">Nachschlagewerk</a>
+						</h3>
+						<p class="text-muted">Die erledigten Arbeiten sind hier
+							dokumentiert.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 text-center">
+					<div class="service-box">
+						<i class="fa fa-4x fa-paper-plane wow bounceIn text-primary"
+							data-wow-delay=".1s"></i>
+						<h3>
+							<a href="" target="_blank">Arbeitsjournal</a>
+						</h3>
+						<p class="text-muted">Hier sind alle Arbeitsschritte und
+							Arbeitsprozesse dokumentiert.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 text-center">
+					<div class="service-box">
+						<i class="fa fa-4x fa-users wow bounceIn text-primary"
+							data-wow-delay=".2s"></i>
+						<h3>
+							<a href="" target="_blank">Berufsparcour</a>
+						</h3>
+						<p class="text-muted">Dummy-Webseite f&uuml;r Berufsparcours.</p>
+						<p class="text-muted">
+							<em>Themen: SQL-Injection, Cross-Site-Scriptig und input Felder</em>.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 text-center">
+					<div class="service-box">
+						<i class="fa fa-4x fa-plane wow bounceIn text-primary"
+							data-wow-delay=".3s"></i>
+						<h3>
+							<a href="http://www.flughafen-zuerich.ch/" target="_blank">Flughafen
+								Z&uuml;rich AG</a>
+						</h3>
+						<p class="text-muted">Link zu der Webseite von der Flughafen
+							Z&uuml;rich AG.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<aside class="bg-dark">
+		<div class="container text-center">
+			<div class="call-to-action">
+				<h2>Informatiker Applikationsentwicklung!</h2>
+				<hr class="primary">
+				<p>Diese Webseite dient zu der &Uuml;bersicht &uuml;ber die
+					verschiedenen Projekte, welche ich in der Lehre und in der Freizeit
+					durchf&uuml;hren werde.</p>
+			</div>
+		</div>
+	</aside>
+
+	<section id="project">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 text-center">
+					<h2 class="section-heading">Projekte</h2>
+					<hr class="primary">
+					<p>F&uuml;r Kunden erstellte Internetauftritte.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="no-padding" id="test">
+		<div class="container-fluid">
+			<div class="row no-gutter">
+				<div class="col-lg-4 col-sm-6">
+					<a href="http://praxis-witzig.ch/" target="_blank"
+						class="portfolio-box"> <img src="img/projects/praxis.jpg"
+						class="img-responsive" alt="">
+						<div class="portfolio-box-caption">
+							<div class="portfolio-box-caption-content">
+								<div class="project-category text-faded">Webseite</div>
+								<div class="project-name">praxis-witzig.ch</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-sm-6">
+					<a href="http://www.malatelier-wiwize.ch/" class="portfolio-box"> <img
+						src="img/projects/wiwize.jpg" class="img-responsive" alt="">
+						<div class="portfolio-box-caption">
+							<div class="portfolio-box-caption-content">
+								<div class="project-category text-faded">Webseite</div>
+								<div class="project-name">malatelier-wiwize.ch</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 col-sm-6">
+					<a href="http://ww.vkazo.ch" class="portfolio-box"> <img
+						src="img/projects/vkazo.jpg" class="img-responsive" alt="">
+						<div class="portfolio-box-caption">
+							<div class="portfolio-box-caption-content">
+								<div class="project-category text-faded">Web-Application</div>
+								<div class="project-name">Abwesenheitstool Vkazo</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-primary" id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 text-center">
+					<div class="row">
+						<h2 class="section-heading">Kontakt</h2>
+						<hr class="light">
+					</div>
+					<div class="row">
+						<div class="col-md-10 col-sm-offset-1">
+							<div class="row">
+								<h3 class="section-heading">Brauchen Sie eine Webseite?
+									Schreiben Sie mir eine Mail!</h3>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<a href="mailto:admin@luca-marti.ch?Subject=Website Anfrage"
+										class="btn btn-default btn-xl"><i class="fa fa-envelope""></i>
+										admin@luca-marti.ch</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<aside class="bg-dark footer">
+		<div class="container text-center">
+			<div class="call-to-action">
+				<p>Copyright &copy; <?php echo date("Y") ?> luca-marti.ch | Luca Marti | <a
+						href="http://startbootstrap.com/" target="_blank">Bootstrap</a> |
+					<a href="imprint.php" target="_blank">Impressum</a>
+				</p>
+			</div>
+		</div>
+	</aside>
+
+
+
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Plugin JavaScript -->
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/jquery.fittext.js"></script>
+	<script src="js/wow.min.js"></script>
+
+	<!-- Custom Theme JavaScript -->
+	<script src="js/creative.js"></script>
+
+</body>
+
+</html>
